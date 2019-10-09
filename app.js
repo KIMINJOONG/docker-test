af519f95d21ad5326fef4c3936406953ccbe01be
KIMINJOONG <ijboym216@gmail.com>
2019-10-09 10:58:00 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = proce.env.PORT || 3000;
+
+app.get('/*', (req, res, next) => {
+    res.send('heelo world');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+});
